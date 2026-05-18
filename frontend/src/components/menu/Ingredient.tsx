5bffb32d47f0ae6d782e83a33d381dfdbf58ce34
lindseysamson5 <lindseysamson5@gmail.com>
2026-05-19 02:15:18 +0800

    refactor: simplify remove button handler in Ingredient component

--- frontend/src/components/menu/Ingredient.tsx
+++ frontend/src/components/menu/Ingredient.tsx
@@ -4,12 +4,16 @@ import type { MenuIngredientDTO } from "../../types/menu.type";
 export default function Ingredient ({ ingredient, remove } : { ingredient: MenuIngredientDTO, remove: (id: string) => void }) {
     const { data } = useGetInventoryById(ingredient.inventory_item_id);
 
+    const handleRemove = () => {
+        remove(ingredient.inventory_item_id)
+    }
+
     if(!data?.inventoryItem) return null;
 
     return (
         <div className="flex justify-between items-center text-xs">
             <p>{data.inventoryItem.name} {ingredient.amount}{ingredient.unit.toUpperCase()}</p>
-            <button className="cursor-pointer" onClick={() => remove(ingredient.inventory_item_id)}>Remove</button>
+            <button className="cursor-pointer" onClick={handleRemove}>Remove</button>
         </div>
     )
 }
