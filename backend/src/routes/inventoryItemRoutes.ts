16215b14318aa4f546121700d2c7a741e38e7c89
lindseysamson5 <lindseysamson5@gmail.com>
2026-05-21 04:19:59 +0800

    feat: add endpoints for total and low stock inventory items

--- backend/src/routes/inventoryItemRoutes.ts
+++ backend/src/routes/inventoryItemRoutes.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
-import { login, refreshAccessToken } from "../controllers/authController";
 import { authenticate, authorizePermission, hasAnyPermission } from "../middlewares/authMiddleware";
 import PERMISSIONS from "../utils/permissions";
-import { createInventoryItem, deleteInventoryItem, getInventoryItemById, getInventoryItems, updateInventoryItem } from "../controllers/inventoryItemController";
+import { createInventoryItem, deleteInventoryItem, getInventoryItemById, getInventoryItems, getTotalInventoryItems, getTotalLowStockInventoryItems, updateInventoryItem } from "../controllers/inventoryItemController";
 
 const router = Router();
 
@@ -18,6 +17,20 @@ router.get(
     authenticate,
     authorizePermission(PERMISSIONS.INVENTORY_READ_ALL),
     getInventoryItems
+)
+
+router.get(
+    '/low-stock',
+    authenticate,
+    authorizePermission(PERMISSIONS.DASHBOARD_VIEW),
+    getTotalLowStockInventoryItems
+)
+
+router.get(
+    '/total',
+    authenticate,
+    authorizePermission(PERMISSIONS.DASHBOARD_VIEW),
+    getTotalInventoryItems
 )
 
 router.put(
